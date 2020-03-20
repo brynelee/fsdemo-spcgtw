7bc506a4add52c01e9129d041e18b32121c013fe
Bryne Lee <bryne_lxd@sina.com>
2020-03-20 23:56:14 +0800

    initialized project

---
+++ src/test/java/com/xdorg1/fsdemospcgtw/FsdemoSpcgtwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xdorg1.fsdemospcgtw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsdemoSpcgtwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
